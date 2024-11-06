ba498ba3e8f47824eb82f62198f126614d6a6f77
alfa03r <muhammadalfajar218@gmail.com>
2024-11-06 11:23:56 +0700

    Update : Adding MCR for Sponsor

---
+++ app/Http/Controllers/SponsorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sponsor;
+use Illuminate\Http\Request;
+
+class SponsorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(sponsor $sponsor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(sponsor $sponsor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, sponsor $sponsor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(sponsor $sponsor)
+    {
+        //
+    }
+}
